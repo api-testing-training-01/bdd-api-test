4c307c5caf4ad176c355af4b3d3f028af88b2c44
marcos-x86 <marcos.x86@outlook.com>
2020-03-27 17:57:21 -0400

    Framework improvements

--- src/test/java/org/fundacionjala/bdd/api/stepdefs/MyStepdefs.java
+++ src/test/java/org/fundacionjala/bdd/api/stepdefs/MyStepdefs.java
@@ -1,12 +1,15 @@
 package org.fundacionjala.bdd.api.stepdefs;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import org.fundacionjala.bdd.api.EnvReader;
+import org.fundacionjala.bdd.api.utils.DynamicIdHelper;
 import org.fundacionjala.bdd.api.utils.Helper;
 
 import java.io.File;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
@@ -19,6 +22,11 @@ public class MyStepdefs {
 
     public MyStepdefs(final Helper helper) {
         this.helper = helper;
+    }
+
+    @Given("I store response as {string}")
+    public void storeResponseAs(final String key) {
+        helper.addResponse(key, response);
     }
 
     @When("I send GET request to {string}")
@@ -40,17 +48,37 @@ public class MyStepdefs {
                 .post(endpoint);
     }
 
+    @When("I send DELETE request to {string} with body")
+    public void iSendDELETERequestToWithBody(final String endpoint) {
+        String processedEndpoint = DynamicIdHelper.buildEndpoint(helper.getResponses(), endpoint);
+        response = given()
+                .header("X-TrackerToken", EnvReader.getInstance().getApiToken())
+                .header("Content-Type", "application/json")
+                .when()
+                .delete(processedEndpoint);
+    }
+
     @Then("Response status code should be {int}")
     public void responseStatusCodeShouldBe(final int expectedStatusCode) {
         int actualStatusCode = response.getStatusCode();
-        String id = response.jsonPath().getString("id");
-        helper.addNewId(id);
         assertEquals(actualStatusCode, expectedStatusCode);
+        if (!response.getBody().asString().isEmpty()) {
+            String id = response.jsonPath().getString("id");
+            helper.addNewId(id);
+        }
     }
 
     @Then("Response body should match with {string} json schema")
     public void responseBodyShouldMatchWithJsonSchema(final String pathSchema) {
         File schemaFile = new File(pathSchema);
         response.then().assertThat().body(matchesJsonSchema(schemaFile));
+    }
+
+    @Then("Response should contain the following data")
+    public void responseShouldContainTheFollowingData(final Map<String, String> expectedData) {
+        for (String key : expectedData.keySet()) {
+            assertEquals(response.jsonPath().getString(key), expectedData.get(key),
+                    String.format("The '%s' field does not match with expected value.", key));
+        }
     }
 }
--- src/test/resources/features/pivotaltests.feature
+++ src/test/resources/features/pivotaltests.feature
@@ -10,7 +10,26 @@ Feature: Projects
   Scenario: Create new project
     When I send POST request to "https://www.pivotaltracker.com/services/v5/projects" with body
     """
-    {"name":"Executioner1"}
+    {
+     "name":"Executioner1",
+     "public": true,
+     "iteration_length": 2
+    }
     """
     Then Response status code should be 200
       And Response body should match with "src/test/resources/schemas/postSchema.json" json schema
+      And Response should contain the following data
+        | name             | Executioner1 |
+        | public           | false        |
+        | iteration_length | 2            |
+
+  @deleteCreatedProject
+  Scenario: Delete project
+    Given I send POST request to "https://www.pivotaltracker.com/services/v5/projects" with body
+    """
+    {"name":"Executioner1"}
+    """
+      And I store response as "PostResponse"
+    Then Response status code should be 200
+    When I send DELETE request to "https://www.pivotaltracker.com/services/v5/projects/{PostResponse.id}" with body
+      Then Response status code should be 204
